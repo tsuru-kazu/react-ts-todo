4d83a18d07addcd869b63082b6b81aacdb1bc9c8
tsurusekazuki <b1714935@planet.kanazawa-it.ac.jp>
2020-06-01 11:35:03 +0900

    add: Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ReactFragment} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import firebase from "firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//screens
+import Home from './screens/Home';
+import Profile from './screens/Profile';
+import SignInOrUp from './screens/SignInOrUp';
+import SignUp from './screens/SignUp';
+
+import Auth from './Auth';
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/signin" component={SignInOrUp}/>
+                <Route exact path="/signup" component={SignUp} />
+                <Auth>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/profile" component={Profile} />
+                        <Route render={() => <p>not found.</p>} />
+                    </Switch>
+                </Auth>
+            </Switch>
+        </Router>
+    )
 }
 
 export default App;
